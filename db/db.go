37029587824a48db213857a59d42c159a94275ad
MicroFish91 <40250218+MicroFish91@users.noreply.github.com>
2024-09-28 21:34:02 -0700

    Fix db connection url

--- db/db.go
+++ db/db.go
@@ -26,7 +26,11 @@ func NewPostgresStorage(dbConfig PostgresDbConfig) (*pgxpool.Pool, error) {
 	if dbConfig.DbUrl != "" {
 		connStr = dbConfig.DbUrl
 	} else {
-		connStr = fmt.Sprintf("postgresql://%s:%s@%s:%s/%s", dbConfig.DbUser, dbConfig.DbPassword, dbConfig.DbHost, dbConfig.DbPort, dbConfig.DbName)
+		var dbLogin string
+		if dbConfig.DbUser != "" && dbConfig.DbPassword != "" {
+			dbLogin = fmt.Sprintf("%s:%s@", dbConfig.DbUser, dbConfig.DbPassword)
+		}
+		connStr = fmt.Sprintf("postgresql://%s%s:%s/%s", dbLogin, dbConfig.DbHost, dbConfig.DbPort, dbConfig.DbName)
 	}
 
 	config, err := pgxpool.ParseConfig(connStr)
